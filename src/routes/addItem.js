a32fe416fbb8eec5211894025fbe6242bb315dae
ashish <ashishmax31@gmail.com>
2026-09-09 19:30:55 +0530

    Introduce conditional todo creation failure for preview review spike

--- src/routes/addItem.js
+++ src/routes/addItem.js
@@ -2,6 +2,10 @@ const db = require('../persistence');
 const {v4 : uuid} = require('uuid');
 
 module.exports = async (req, res) => {
+    if (req.body.name.length > 80) {
+        return res.status(500).json({ message: 'Unable to add item' });
+    }
+
     const item = {
         id: uuid(),
         name: req.body.name,
